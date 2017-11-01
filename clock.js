080ee11839c6c46cb609beb9e413252ef886fb64
davirk21 <davirk21@gmail.com>
2017-11-01 11:46:17 -0700

    editing clock.js

--- clock.js
+++ clock.js
@@ -5,17 +5,12 @@ var gen = require('./generate/generator.js')
 
 // gov.worker1();
 
-var job = new CronJob(
-  '00 00 00 * * 7', 
-  function() {
-    console.log('worker is starting');
-    gov.worker1();
-  },
-  function() {
-    console.log("worker is finish");
-  }, 
-  true, 
-  'America/Los_Angeles');
+var job = new CronJob({
+  cronTime: '00 00 00 * * 7', 
+  onTick: gov.worker1,
+  start: true, 
+  timeZone: 'America/Los_Angeles'
+});
 
 job.start()
 console.log('fire instance clock job status', job.running);
